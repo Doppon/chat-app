d60d4175c81b25b3f7f7eb9b5004ae758852bbd7
Doppo Sekino <zaxpkuraudo7777@gmail.com>
2018-03-07 20:37:43 +0900

    アクションケーブルを有効に

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'rooms#show'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ここでAction Cableを有効にしている
+  mount ActionCable.server => '/cable'
 end
